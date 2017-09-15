8d758d24285a0a11e7dd050d927e86624e348568
Matt <mattm4300@gmail.com>
2017-09-15 05:55:24 +0000

    Set baud rate to highest possible and it worked.

--- Source/settings.h
+++ Source/settings.h
@@ -3,11 +3,12 @@
 #define _SETTINGS_H_
 
 /* Serial Interface Settings
- *
+ * Notes: Common baud rates are multiples of 1200
+ *        in the range of 1200 to 115200.
  */
 #define INTERFACE "/dev/serial0"
 #define IDENTIFIER_LENGTH 50
-#define BAUD_RATE 9600
+#define BAUD_RATE 115200
 #define CHECKSUM_ENABLED 0
 
 /* Transfer Protocol Settings
