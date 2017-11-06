2d32f1a0ca059ff3e4b7270faef9f83d3f01bd62
Matt Morgan <mattm4300@gmail.com>
2017-11-06 15:02:01 -0600

    Added RF settings to settings file.

--- src/settings.h
+++ src/settings.h
@@ -2,17 +2,15 @@
 #ifndef _SETTINGS_H_
 #define _SETTINGS_H_
 
-/* Serial Interface Settings
- * Notes: Common baud rates are multiples of 1200
- *        in the range of 1200 to 115200.
- */
+// Serial Interface Settings
 #define INTERFACE "/dev/serial0"
 #define IDENTIFIER_LENGTH 32
 #define BAUD_RATE 38400
 
-/* Transfer Protocol Settings
- *
- */
-#define COMPRESS 0
+// RF Interface Settings
+#define RF_FREQUENCY 868.00
+#define RF_NODE_ID 1
+#define TX_POWER 14
+#define BOARD_LORASPI // For RadioHead/..../RasPiBoards.h
 
 #endif
