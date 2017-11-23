c6309aa7f37f87574a751846ac4b577ad28f9350
Matt M <mattm4300@gmail.com>
2017-11-23 04:15:57 +0000

    Still working on RF.

--- src/temp/send.cpp
+++ src/temp/send.cpp
@@ -56,13 +56,11 @@ int main(int argc, const char **argv) {
     uint8_t len = sizeof(data);
     
     while(true) {
-        if(manager.available()) {
-            if(manager.sendtoWait(data, len, SERVER)) {
-                printf("Sent!");
-                bcm2835_close();
-                return 0;
-            }
-            printf("%d\n", manager.retries());
+        if(manager.sendtoWait(data, len, SERVER)) {
+            printf("Sent!");
+            bcm2835_close();
+            return 0;
         }
+        printf("%d\n", manager.retries());
     }
 }
