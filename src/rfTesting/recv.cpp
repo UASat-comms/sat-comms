00196060754fe0743e637ec433f49d0e2bd498cb
Matt Morgan <mattm4300@gmail.com>
2017-12-07 15:40:23 -0600

    Updated modular RF testing files to use modular functions.

--- src/rfTesting/recv.cpp
+++ src/rfTesting/recv.cpp
@@ -1,57 +1,17 @@
-#include <bcm2835.h>
 #include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <RH_RF95.h>
+#include "rf.hpp"
 #include "settings.h"
 #include "easylogging++.h"
 
 INITIALIZE_EASYLOGGINGPP
 
 el::Configurations conf(LOGCONFIG);
-RH_RF95 rf95(RF_CS_PIN, RF_IRQ_PIN);
 
 int main() {
     el::Loggers::reconfigureAllLoggers(conf);
-     LOG(INFO) << "Starting program";
-
-     if(!bcm2835_init()) {
-          LOG(FATAL) << "bcm2835 failed to initialize.";
-     }
-
-     pinMode(RF_RST_PIN, OUTPUT);
-     digitalWrite(RF_RST_PIN, HIGH);
-     // manual reset
-     digitalWrite(RF_RST_PIN, LOW);
-     delay(10);
-     digitalWrite(RF_RST_PIN, HIGH);
-     delay(10);
-
-     if(!rf95.init()) {
-          LOG(FATAL) << "rf95 driver failed to initialize.";
-     }
-
-     if(!rf95.setFrequency(RF_FREQUENCY)) {
-          LOG(FATAL) << "rf95 frequency failed to be set.";
-     }
-     
-     rf95.setTxPower(23, false);
-
-    while(1) {
-        if(rf95.available()) {
-            uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-            uint8_t len = sizeof(buf);
-            if(rf95.recv(buf, &len)) {
-                LOG(DEBUG) << "MESS_LEN: <" << (int) len << ">";
-                LOG(DEBUG) << "MESS: " << buf;
-                break;
-            } else {
-                LOG(FATAL) << "recv failed!";
-            }
-        }
-        LOG(INFO) << "No message yet..";
-        bcm2835_delay(500);
-    }
-    bcm2835_close();
-     return 0;
+    setupRF();
+    rfMessage *m;
+    m = recvRF();
+    LOG(INFO) << "Message received: <" << m->data << ">";
+    return 0;
 }
--- src/rfTesting/send.cpp
+++ src/rfTesting/send.cpp
@@ -1,50 +1,17 @@
-#include <bcm2835.h>
 #include <stdio.h>
-#include <signal.h>
-#include <unistd.h>
-#include <RH_RF95.h>
+#include "rf.hpp"
 #include "settings.h"
 #include "easylogging++.h"
 
 INITIALIZE_EASYLOGGINGPP
 
 el::Configurations conf(LOGCONFIG);
-RH_RF95 rf95(RF_CS_PIN, RF_IRQ_PIN);
 
 int main() {
-    el::Loggers::reconfigureAllLoggers(conf);
-     LOG(INFO) << "Starting program";
-
-     if(!bcm2835_init()) {
-          LOG(FATAL) << "bcm2835 failed to initialize.";
-     }
-
-     pinMode(RF_RST_PIN, OUTPUT);
-     digitalWrite(RF_RST_PIN, HIGH);
-     // manual reset
-     digitalWrite(RF_RST_PIN, LOW);
-     delay(10);
-     digitalWrite(RF_RST_PIN, HIGH);
-     delay(10);
-
-     if(!rf95.init()) {
-          LOG(FATAL) << "rf95 driver failed to initialize.";
-     }
-
-     if(!rf95.setFrequency(RF_FREQUENCY)) {
-          LOG(FATAL) << "rf95 frequency failed to be set.";
-     }
-     
-     rf95.setTxPower(23, false);
-     
-     uint8_t data[] = "test";
-     uint8_t len = sizeof(data);
-
-     LOG(DEBUG) << "Starting packet transmission.";
-     rf95.send(data, len);
-     LOG(DEBUG) << "Waiting until transmission finishes..";
-     rf95.waitPacketSent();
-     LOG(DEBUG) << "Packet sent.";
-
+     el::Loggers::reconfigureAllLoggers(conf);
+     setupRF();
+     char data[] = "testtesttest";
+     rfMessage *m = createRFMessage((char *) data);
+     sendRF(m);
      return 0;
 }
