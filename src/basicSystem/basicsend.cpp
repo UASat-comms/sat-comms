b93f3ecf09909a96fc495739dd11c89791c3a036
Matt M <mattm4300@gmail.com>
2017-12-27 15:51:40 -0600

    Finsihed skeleton of basic send side.

--- src/basicSystem/basicsend.cpp
+++ src/basicSystem/basicsend.cpp
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <cstdio>
 #include "rf.hpp"
 #include "settings.h"
@@ -7,19 +10,10 @@
 
 using namespace std;
 
+// Required macro to initialize the easylogging module.
 INITIALIZE_EASYLOGGINGPP
 
-std::string getSHA256Hash(const char *str) {
-  std::stringstream s;
-  s << str;
-  return picosha2::hash256_hex_string(s.str());
-}
-
-std::string getSHA256Hash(std::string str) {
-  return picosha2::hash256_hex_string(str);
-}
-
-void setup() {    
+void setup() {
     // Configure logging module.
     el::Configurations conf(LOGCONFIG);
     el::Loggers::reconfigureAllLoggers(conf);
@@ -29,14 +23,56 @@ void setup() {
     setupRF();
 }
 
+std::string getFileChecksum(const char *fname) {
+     ifstream myfile(fname);
+     stringstream mystr;
+     string line;
+
+     LOG(DEBUG) << "Reading file data to get SHA256 checksum...";
+     if(myfile.is_open()) {
+          while(getline(myfile, line)) {
+               mystr << line << endl;
+          }
+     } else {
+          LOG(FATAL) << "Error reading file data for checksum.";
+     }
+     return picosha2::hash256_hex_string(mystr.str());
+}
+
 int main(int argc, const char **argv) {
     setup();
-    
+
     // Make sure we enter the name of file we want to send.
     if(argc < 2) {
         LOG(FATAL) << "Must enter name of file to send.";
     }
-           
 
+    // Get the file checksum and transmit it via RF.
+    string checksum = getFileChecksum(argv[1]);
+    LOG(DEBUG) << "File checksum is: " << checksum;
+    sendRF((char *) checksum.c_str());
+    LOG(INFO) << "File checksum transmitted."
+    bcm2835_delay(3000);
+
+    // Transmit the file via serial.
+    transmitFile(argv[1]);
+
+    // See if the checksum matches. Try again until correct.
+    char *reply = recvRF();
+    int tryCount = 0;
+    int success = 0;
+    while((strcmp(reply, "GOOD") != 0) && tryCount < TRY_LIMIT) {
+         bcm2835_delay(1000);
+         transmitFile(argv[1]);
+         ++tryCount;
+         reply = recvRF();
+         if(strcmp(reply, "GOOD") == 0) success = 1;
+    }
+
+    if(success) {
+         LOG(INFO) << "File transmission was successful!";
+    } else {
+         LOG(INFO) << "File transmission failed!";
+    }
     return 0;
 }
