f07f61e3cf27bcb32d2cc3a4e01909e1b01ff0ab
Matt <mattm4300@gmail.com>
2018-01-16 20:13:33 +0000

    Made both RF test scripts attempt 50 iterations.

--- src/rfTesting/sendtestscript.sh
+++ src/rfTesting/sendtestscript.sh
@@ -1,7 +1,7 @@
 echo "=================================="
 echo "----  Start of RF Send Test   ----"
 
-for k in `seq 1 10`;
+for k in `seq 1 50`;
 do
     sleep 2
     echo "=====> Test iteration no.: $k"
