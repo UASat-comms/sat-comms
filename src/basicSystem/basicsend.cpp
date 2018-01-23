4571d6fe118c01393a01d92a1d62eb0630c211fe
Matt Morgan <mattm4300@gmail.com>
2018-01-23 16:02:06 -0600

    Fixed syntax issue.

--- src/basicSystem/basicsend.cpp
+++ src/basicSystem/basicsend.cpp
@@ -6,9 +6,13 @@
 #include <cstddef>
 
 #include "schifra_galois_field.hpp"
+#include "schifra_galois_field_polynomial.hpp"
 #include "schifra_sequential_root_generator_polynomial_creator.hpp"
 #include "schifra_reed_solomon_encoder.hpp"
-#include "schifra_reed_solomon_file_encoder.hpp"
+#include "schifra_reed_solomon_decoder.hpp"
+#include "schifra_reed_solomon_block.hpp"
+#include "schifra_error_processes.hpp"
+#include "schifra_reed_solomon_file_encoder"
 
 #include "rf.hpp"
 #include "settings.h"
